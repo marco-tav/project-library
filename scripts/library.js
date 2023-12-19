6988c2a4cd16e9b9ca71d8f126c71bb00053ec49
marco-tav <marco.tavella@outlook.com>
2023-12-19 10:51:07 -0300

    Add input elements references in jS and an array to store books

--- scripts/library.js
+++ scripts/library.js
@@ -1,3 +1,12 @@
+const bookName = document.getElementById('book-title');
+const bookAuthor = document.getElementById('book-author');
+const bookGenre = document.getElementById('book-genre');
+const bookPages = document.getElementById('book-pages');
+const bookRead = document.getElementById('book-read');
+const submitBtn = document.getElementById('subit');
+
+const bookArray = [];
+
 function Book(title, author, genre, pages, read = false) {
   this.title = title;
   this.author = author;
